b4b00974ff0fe3a39d30775b26bf6c21a66da4bc
cglass <cglass@sfu.ca>
2019-10-03 13:55:32 -0700

    Adds assets to project, trying to add a projectile

---
+++ Cyberview/Assets/Scripts/Item/Missile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public Vector2 initialVelocity = new Vector2(100,0);
+    private Rigidbody2D body2d;
+
+    private void Awake(){
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body2d.velocity = new Vector2(initialVelocity.x, initialVelocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
