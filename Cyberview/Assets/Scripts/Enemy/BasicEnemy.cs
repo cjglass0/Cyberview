1f50a3834dec15258ac13d6aaea9bb41ba476b6c
cglass <cglass@sfu.ca>
2019-10-05 18:42:56 -0700

    Fully functional enemies, several placed within the test level.

--- Cyberview/Assets/Scripts/Enemy/BasicEnemy.cs
+++ Cyberview/Assets/Scripts/Enemy/BasicEnemy.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class BasicEnemy : MonoBehaviour
 {
     public int health = 3;
+    public bool right = true;
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var body2d = GetComponent<Rigidbody2D>();
+        body2d.velocity = new Vector2(speed,0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.rotation = Quaternion.Euler(0,0,0);
+        var body2d = GetComponent<Rigidbody2D>();
+        //if hitting something that stops movement (like a wall) then the object will turn around
+        if(Mathf.Abs(body2d.velocity.x) < 1){
+            right = !right;
+        }
+
+        
+
+        if(right){
+            body2d.velocity = new Vector2(speed,0);
+        }
+        else{
+            body2d.velocity = new Vector2(-speed,0);
+        }
+        
         if(health <= 0){
             Destroy(gameObject);
         }
@@ -22,4 +41,5 @@ public class BasicEnemy : MonoBehaviour
     public void HitBy(GameObject projectile){
         health--;
     }
+
 }
