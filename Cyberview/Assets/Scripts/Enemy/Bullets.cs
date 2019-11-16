7e3916b53665a5125e394c1628fddfc4207f0545
Rebecca <rebeccasingh98@gmail.com>
2019-11-15 20:44:46 -0800

    just saving changes

--- Cyberview/Assets/Scripts/Enemy/Bullets.cs
+++ Cyberview/Assets/Scripts/Enemy/Bullets.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
-	float bulletSpeed = 5f;
+	float bulletSpeed = 7f;
 	Rigidbody2D rigidbody2d;
-	private Transform target; //holds the enemies target
+	PlayerManager target; //holds the enemies target
 	Vector2 moveHere;
     // Start is called before the first frame update
     void Start()
     {
     	rigidbody2d = GetComponent<Rigidbody2D>();
-    	target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
+    	target = GameObject.FindObjectOfType<PlayerManager>(); 
     	moveHere = (target.transform.position - transform.position).normalized * bulletSpeed;
     	rigidbody2d.velocity = new Vector2(moveHere.x, moveHere.y);
     	Destroy (gameObject, 3f);
@@ -20,7 +20,7 @@ public class Bullets : MonoBehaviour
     }
 
     void OnTriggerEnter2D (Collider2D col){
-    	if (col.gameObject.name.Equals("player")){
+    	if (col.gameObject.name.Equals("PlayerManager")){
     		Destroy (gameObject);
     	}
     }
@@ -30,3 +30,4 @@ public class Bullets : MonoBehaviour
         
     }
 }
+
