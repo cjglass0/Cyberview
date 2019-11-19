d8e053fb4f7f57f1c4d05fefefd88e5372b7f092
robert-michels <rmichels@sfu.ca>
2019-11-18 16:00:09 -0800

    Minor Bug Fix

--- Cyberview/Assets/Scripts/Managers/GameManager.cs
+++ Cyberview/Assets/Scripts/Managers/GameManager.cs
@@ -43,7 +43,7 @@ public class GameManager : MonoBehaviour
         //Set up stuff for Pausing
         Time.timeScale = 1;
         paused = false;
-        playerDied = true;
+        //playerDied = true;
 
         //Set curScene to be active Scene (_Base)
         curScene = SceneManager.GetActiveScene();
